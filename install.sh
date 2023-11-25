a3826fd527a8acde0ed7e014e9587cda317ad868
Rutvik Soni <rutviksoni222@gmail.com>
2023-11-26 03:32:55 +0530

    removed sudo

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo mkdir /etc/mtwa
-sudo mkdir /var/www/html/appdemo
-sudo cp mtwa.conf /etc/mtwa/mtwa.conf
-sudo cp html/* /var/www/html/appdemo/
-sudo cp scripts/* /var/www/html/appdemo/
+mkdir /etc/mtwa
+mkdir /var/www/html/appdemo
+cp mtwa.conf /etc/mtwa/mtwa.conf
+cp html/* /var/www/html/appdemo/
+cp scripts/* /var/www/html/appdemo/
 
 echo "Update complete!"
 
